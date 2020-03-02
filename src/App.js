497aa45f8348fb644766f55d882487f4988b9887
vidushaNasome <vnasome58@gmail.com>
2020-03-02 21:37:09 +0530

    hell

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={ApiRespose:""};
+  }
+
+  CallAPI(){
+    fetch("http://localhost:9000/test")
+        .then(res=>res.test())
+        .then(res=>this.setState({ApiResponse:res}));
+  }
+  componentWillMount(){
+    this.callAPI();
+  }
+
+ render(){
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +26,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
+      <p>{this.state.ApiResponse}</p>
+
     </div>
   );
+
+
+}
 }
 
 export default App;
